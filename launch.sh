dd22f51ab56002dbb1c79ce096808970e0232cff
Creed is DeaD <0.creed.is.dead.0@gmail.com>
2016-10-26 08:06:16 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -125,6 +125,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+rm -r ../.telegram-cli/state #Prevent tg from crash
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/creedbot.lua -l 1 -E $@
 fi
